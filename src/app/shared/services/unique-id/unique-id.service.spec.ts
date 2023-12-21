137d954f2257caf39cc1da02caec8d4635dc4091
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-21 10:19:57 -0300

    UniqueIdService.spec first Jasmine test creation
    #generateUniqueIdWithPrefix should generate id when called with prefix

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,17 @@
+import { UniqueIdService } from './unique-id.service';
+
+// describe('O artefato que queremos testar', () => {
+//   it('Primeira condição que queremos testar', () => {});
+
+//   it('Segunda condição que queremos testar', () => {});
+
+//   it('blablabla should blablabla when blablabla', () => {});
+// });
+
+describe('UniqueIdService', () => {
+  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+    const service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id).toContain('app-');
+  });
+});
