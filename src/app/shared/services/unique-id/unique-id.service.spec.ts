20326bdf5d6189ddc7c19b7790faefb21472b592
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-21 10:48:48 -0300

    Test patterns: Artefact name and methods name

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -8,8 +8,16 @@ import { UniqueIdService } from './unique-id.service';
 //   it('blablabla should blablabla when blablabla', () => {});
 // });
 
-describe('UniqueIdService', () => {
-  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+// describe('UniqueIdService', () => {
+//   it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+//     const service = new UniqueServiceId();
+//     const id = service.generateUniqueIdWithPrefix('app');
+//     expect(id).toContain('app-');
+//   });
+// });
+
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id).toContain('app-');
