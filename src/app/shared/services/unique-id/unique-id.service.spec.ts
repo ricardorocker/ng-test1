ed0129399833a78d42ec61214cab2e1ca4d1fc0c
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-21 14:45:08 -0300

    Test(duplicate id): new Set, for, add, size, toBe

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -22,4 +22,13 @@ describe(UniqueIdService.name, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
 });
