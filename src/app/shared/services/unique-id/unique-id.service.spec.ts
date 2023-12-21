99d20ddd3e963299107307e896929fa8b0084a76
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-21 15:07:05 -0300

    beforeEach "it" instamce new artefact

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -17,14 +17,17 @@ import { UniqueIdService } from './unique-id.service';
 // });
 
 describe(UniqueIdService.name, () => {
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix('app'));
@@ -33,7 +36,6 @@ describe(UniqueIdService.name, () => {
   });
 
   it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
-    const service = new UniqueIdService();
     service.generateUniqueIdWithPrefix('app');
     service.generateUniqueIdWithPrefix('app');
     expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
