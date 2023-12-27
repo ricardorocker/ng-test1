21d058de9f87f3a202a7f72d33595baac41252b3
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-12-27 11:24:53 -0300

    validId with regEx on service

--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -3,10 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UniqueIdService {
-  private numberOfGeneratedIds = 0;
+  private numberOfGeneratedIds = 0; 
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
   public generateUniqueIdWithPrefix(prefix: string): string {
-    if (!prefix) throw Error('Prefix can not be empty');
+    if (!prefix || !this.validId.test(prefix)) throw Error('Prefix can not be empty');
     const uniqueId = this.generateUniqueId();
     this.numberOfGeneratedIds++;
     return `${prefix}-${uniqueId}`;
